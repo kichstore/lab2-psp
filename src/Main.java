2e098c35a2a6ee1a000fd77d257c7b8cd956a343
kichstore <kostya10ton@mail.ru>
2023-12-17 17:17:18 +0300

    com

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("I am new commit");
         System.out.println("Я работаю в новой ветке и комит");
+        System.out.println("new news");
     }
 }
