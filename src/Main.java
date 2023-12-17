eb6c5829f6348fdb06c418985236eeab09390287
kichstore <kostya10ton@mail.ru>
2023-12-17 17:09:53 +0300

    commit for new_branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("I am new commit");
+        System.out.println("Я работаю в новой ветке и комит");
     }
 }
